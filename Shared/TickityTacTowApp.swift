2aade6ac692baa1658c2fbf81f1457b81fc9208b
Jared Davidson (Archetapp) <archetapp@gmail.com>
2021-05-28 14:33:56 -0600

    Initial Commit

---
+++ Shared/TickityTacTowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickityTacTowApp.swift
+//  Shared
+//
+//  Created by Jared Davidson on 5/28/21.
+//
+
+import SwiftUI
+
+@main
+struct TickityTacTowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
